7e625ec03dc22381941f7428d40320dc0b499319
Nathaniel <ncotton6@gmail.com>
2016-04-23 14:50:51 -0400

    initial commit

---
+++ src/main/java/edu/rit/csci729/assign3/configuration/Initializer.java
@@ -0,0 +1,29 @@
+package edu.rit.csci729.assign3.configuration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * This is utilized by the web-server to initialize the app.
+ * 
+ * @author Nathaniel Cotton
+ *
+ */
+public class Initializer implements WebApplicationInitializer{
+
+	public void onStartup(ServletContext container) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(Configuration.class);
+		ctx.setServletContext(container);
+		ServletRegistration.Dynamic servlet = container.addServlet(
+				"dispatcher", new DispatcherServlet(ctx));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+
+}
